35465d121171ed896a0c9d3d79888c85ffc19f4f
Abhishek Dwivedi <abhishekdwivedi56@yahoo.com>
2021-06-03 18:23:32 +0530

    Added scaffolding and initial self-signed cert-generation code

---
+++ cmd/cert-generator/root.go
@@ -0,0 +1,23 @@
+package cert_generator
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "cert-generator",
+	Short: "cert-generator generates/rotates certs for secure CockroachDB mode",
+	Long:  `cert-generator is a tool used to generate or rotate CA cert, Node cert and Client cert`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ pkg/resource/tls_secret_test.go
@@ -0,0 +1,97 @@
+/*
+Copyright 2021 The Cockroach Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resource_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/cockroachdb/helm-charts/pkg/kube"
+	"github.com/cockroachdb/helm-charts/pkg/resource"
+	"github.com/cockroachdb/helm-charts/pkg/testutils"
+)
+
+func TestLoadTLSSecret(t *testing.T) {
+	ctx := context.TODO()
+	scheme := testutils.InitScheme(t)
+	fakeClient := testutils.NewFakeClient(scheme)
+	r := resource.NewKubeResource(ctx, fakeClient, "test-namespace", kube.DefaultPersister)
+
+	_, err := resource.LoadTLSSecret("non-existing", r)
+	assert.True(t, apierrors.IsNotFound(err))
+}
+
+func TestTLSSecretReady(t *testing.T) {
+	ctx := context.TODO()
+	scheme := testutils.InitScheme(t)
+	name := "test-secret"
+	namespace := "test-namespace"
+
+	tests := []struct {
+		name     string
+		secret   runtime.Object
+		expected bool
+	}{
+		{
+			name: "secret missing required fields",
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      name,
+					Namespace: namespace,
+				},
+				Data: map[string][]byte{
+					"someKey": {},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "secret has all required fields",
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      name,
+					Namespace: namespace,
+				},
+				Data: map[string][]byte{
+					"ca.crt":  {},
+					"tls.crt": {},
+					"tls.key": {},
+				},
+			},
+			expected: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fakeClient := testutils.NewFakeClient(scheme, tt.secret)
+			r := resource.NewKubeResource(ctx, fakeClient, namespace, kube.DefaultPersister)
+
+			actual, err := resource.LoadTLSSecret(name, r)
+			require.NoError(t, err)
+			assert.Equal(t, tt.expected, actual.Ready())
+
+		})
+	}
+}
