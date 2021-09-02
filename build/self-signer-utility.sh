0225c0d283f816a582a762fb7b594f56ef1a7c44
Prafull Ladha <prafull.ladha@gmail.com>
2021-09-02 19:55:44 +0530

    Post jobs to not fail the jobs with no tag change (#163)

--- build/self-signer-utility.sh
+++ build/self-signer-utility.sh
@@ -5,7 +5,7 @@ echo "Your current tag is ${tag}"
 currentCommit=$(git rev-parse HEAD)
 lastCommit=$(git rev-parse @~)
 
-git diff "${lastCommit}" "${currentCommit}" cockroachdb/values.yaml | grep "$tag" | grep +
+git diff "${lastCommit}" "${currentCommit}" cockroachdb/values.yaml | grep -w "$tag" | grep +
 if [[ $? -ne 0 ]]; then
   echo "You have not changed the tag of selfSigner utility"
   exit 1
