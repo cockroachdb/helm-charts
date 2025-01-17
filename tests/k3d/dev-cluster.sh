70f30fa9c8bec74b6809e566be7b7a2dbd315e6b
Josh Carp <jm.carp@gmail.com>
2025-01-17 11:23:54 -0500

    Check in initial e2e tests for cloud operator.
    Add an e2e test that installs the operator, then a crdbcluster, and
    makes assertions about basic crdb functionality.

--- tests/k3d/dev-cluster.sh
+++ tests/k3d/dev-cluster.sh
@@ -4,35 +4,34 @@ zones=3
 
 K3D_PATH="./bin/k3d"
 
-if [ $# -eq 0 ]
-  then
-      echo "No arguments supplied: "
-      echo "  up: Start cluster."
-      echo "     --nodes x: The cluster should have x nodes (default 1)"
-      echo "     --version x: The version of Kubernetes (default 1.24.14)"
-      echo "     --name x: The name of the cluster (default local)"
-      echo "     --network_name x: The name of the cluster's network (default k3d-\${name})"
-      echo "     --region x: The name of the cluster's region for node labels topology.kubernetes.io/region (default us-east-1)"
-      echo "     --zones x: The number of zones in the region for node labels topology.kubernetes.io/zone (default 3)"
-      
-      echo "  down: Delete cluster."
+if [ $# -eq 0 ]; then
+  echo "No arguments supplied: "
+  echo "  up: Start cluster."
+  echo "     --nodes x: The cluster should have x nodes (default 1)"
+  echo "     --version x: The version of Kubernetes (default 1.24.14)"
+  echo "     --name x: The name of the cluster (default local)"
+  echo "     --network_name x: The name of the cluster's network (default k3d-\${name})"
+  echo "     --region x: The name of the cluster's region for node labels topology.kubernetes.io/region (default us-east-1)"
+  echo "     --zones x: The number of zones in the region for node labels topology.kubernetes.io/zone (default 3)"
 
-      exit 1
+  echo "  down: Delete cluster."
+
+  exit 1
 fi
 
 COMMAND="${1-}"
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 nodes=${environment:-1}
-version=${version:-1.24.14}
+version=${version:-1.31.2}
 
 while [ $# -gt 0 ]; do
 
-   if [[ $1 == *"--"* ]]; then
-        param="${1/--/}"
-        declare $param="$2"
-        # echo $1 $2 // Optional to see the parameter:value result
-   fi
+  if [[ $1 == *"--"* ]]; then
+    param="${1/--/}"
+    declare $param="$2"
+    # echo $1 $2 // Optional to see the parameter:value result
+  fi
 
   shift
 done
@@ -48,7 +47,7 @@ set_node_labels() {
   local labels=""
   local az=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
 
-  for ((i=0; i<nodes; i++)); do
+  for ((i = 0; i < nodes; i++)); do
     zone="${region}${az[$((i % zones))]}"
     labels+="--k3s-node-label topology.kubernetes.io/zone=${zone}@agent:${i} "
     labels+="--k3s-node-label topology.kubernetes.io/region=${region}@agent:${i} "
@@ -58,21 +57,21 @@ set_node_labels() {
 }
 
 case $COMMAND in
-  up)
-    node_labels=$(set_node_labels ${nodes} ${region} ${zones})
-    ${K3D_PATH} cluster create ${name} \
-      --network ${network_name} \
-			--registry-config "$SCRIPT_DIR/registries.yaml" \
-			--image rancher/k3s:v${version}-k3s1 \
-			--agents ${nodes} \
-      --k3s-node-label "topology.kubernetes.io/region=${region}@server:0" \
-      ${node_labels} 
+up)
+  node_labels=$(set_node_labels ${nodes} ${region} ${zones})
+  ${K3D_PATH} cluster create ${name} \
+    --network ${network_name} \
+    --registry-config "$SCRIPT_DIR/registries.yaml" \
+    --image rancher/k3s:v${version}-k3s1 \
+    --agents ${nodes} \
+    --k3s-node-label "topology.kubernetes.io/region=${region}@server:0" \
+    ${node_labels}
   ;;
-  down)
-    ${K3D_PATH} cluster delete ${name}
+down)
+  ${K3D_PATH} cluster delete ${name}
   ;;
-  *)
-    echo "Unknown command: $COMMAND"
-    exit 1;
+*)
+  echo "Unknown command: $COMMAND"
+  exit 1
   ;;
 esac
